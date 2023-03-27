eee475b7d0b955ce48f555036d58bb9cdb46fb3e
fredmeteor <fredpeter09899@gmail.com>
2023-03-28 00:53:40 +0300

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+console.log("Hello");
+console.log("Apples");
+console.log("This is a statement");
+console.log("This is also a statement");
