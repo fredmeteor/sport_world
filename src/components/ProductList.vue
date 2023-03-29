e3847339ded188607c67b825ffd3c2171ca2923b
fredmeteor <fredpeter09899@gmail.com>
2023-03-30 01:28:26 +0300

    Added PageControls Component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -9,14 +9,22 @@
             </h4>
           <div class="card-text bg-white p-1">{{ p.description }}</div>
         </div>
+        <!-- <page-controls /> -->
       </div>
     </template>
     
     <script>
-    import { mapState } from "vuex";
+    
+    import { mapGetters} from "vuex";
+    // import PageControls from "./PageControls";
     export default {
+
+      //  components: { PageControls },
+
        computed: {
-        ...mapState(["products"])
+        
+        ...mapGetters({ products: "processedProducts" })
+        
       },filters: {
         currency(value) {
        return new Intl.NumberFormat("en-US",
