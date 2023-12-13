d04aed54325b2bf14066bd342eca4af2373cd8d5
fredmeteor <fredpeter09899@gmail.com>
2023-12-13 15:06:57 +0300

    Added Modified Admin Component

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,3 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="bg-danger text-white text-center h4 p-2">Admin Features</div>
-    </template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-secondary text-white">
+                <a class="navbar-brand">SPORTS STORE Admin</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-secondary p-2">
+                <router-link to="/admin/products" class="btn btn-block btn-primary" 
+                        active-class="active">
+                    Products
+                </router-link>
+                <router-link to="/admin/orders"  class="btn btn-block btn-primary" 
+                        active-class="active">
+                    Orders
+                </router-link>                          
+            </div>
+            <div class="col-9 p-2">
+                <router-view />
+            </div>
+        </div>
+    </div>
+</template>
